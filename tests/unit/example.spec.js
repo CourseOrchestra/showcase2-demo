b7dcd2e5973d6c5c857de53328eb8e4ba0575590
Boris <toborto@yandex.ru>
2022-05-17 18:27:26 +0300

    Подправил модульный тест

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,25 @@
-import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuetify from "vuetify";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+Vue.use(Vuetify);
+
+const localVue = createLocalVue();
+
 describe("HelloWorld.vue", () => {
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "Welcome to Vuetify";
     const wrapper = shallowMount(HelloWorld, {
+      localVue,
+      vuetify,
+
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
