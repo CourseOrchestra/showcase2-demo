5eea0237f608c06d6bf0e9234e0dc1aac1393e8a
Boris <toborto@yandex.ru>
2022-05-18 12:59:27 +0300

    Подправил интеграционные тесты

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,12 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("e2e тесты", () => {
+  it("Начальная страница", () => {
     cy.visit("/");
     cy.contains("span", "Эмблема");
+  });
+  it("Эмблема Платформы КУРС", () => {
+    cy.visit("/emblem");
+    //cy.contains("div", "curs.ru");
   });
 });
