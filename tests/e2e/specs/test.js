b0e7d70bb2f8bece5b0a1dc857aa4cbc9cb17bfc
Boris <toborto@yandex.ru>
2022-05-18 14:38:28 +0300

    Скорректировал e2e тесты

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,7 @@
 describe("e2e тесты", () => {
   it("Начальная страница", () => {
     cy.visit("/");
-    cy.contains("span", "Эмблема");
+    cy.contains("span", "Эмблема Платформы КУРС");
     cy.contains("span", "Навигатор");
     cy.contains("span", "Грид");
     cy.contains("span", "Состояние");
@@ -15,14 +15,14 @@ describe("e2e тесты", () => {
   });
   it("Навигатор", () => {
     cy.visit("/navigator");
-    cy.contains("h1", "навигатор");
+    cy.contains("h1", "Демонстрация навигатора");
   });
   it("Грид", () => {
     cy.visit("/grid");
-    cy.contains("h1", "грид");
+    cy.contains("h1", "Демонстрация грида");
   });
   it("Состояние в url", () => {
     cy.visit("/urlstate");
-    cy.contains("h1", "состояния");
+    cy.contains("h1", "Демонстрация состояния в url");
   });
 });
