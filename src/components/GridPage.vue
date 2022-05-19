be9e4e5f0e3ab4352a875e1f510fdb8b79e8f21c
Boris <toborto@yandex.ru>
2022-05-19 19:09:47 +0300

    Правки по эмблеме КУРСа 5

--- src/components/GridPage.vue
+++ src/components/GridPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container>
+  <v-container fill-height fluid>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Демонстрация грида</h1>
+        <h1 class="display-3 font-weight-bold mb-3">Демонстрация грида</h1>
       </v-col>
     </v-row>
   </v-container>
