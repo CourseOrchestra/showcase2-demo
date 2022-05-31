88f6809a177f91fe5f280735211b5450d2d8d42d
Boris <toborto@yandex.ru>
2022-05-31 18:46:35 +0300

    Отладка ci

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-btn href="/" text>
+      <v-btn href="." text>
         <span class="mr-2">Welcome</span>
       </v-btn>
-      <v-btn href="/logocurs" text>
+      <v-btn href="logocurs" text>
         <span class="mr-2">Логотип КУРСа</span>
       </v-btn>
-      <v-btn href="/logoplatform" text>
+      <v-btn href="logoplatform" text>
         <span class="mr-2">Логотип Платформы</span>
       </v-btn>
-      <v-btn href="/navigator" text>
+      <v-btn href="navigator" text>
         <span class="mr-2">Навигатор</span>
       </v-btn>
-      <v-btn href="/grid" text>
+      <v-btn href="grid" text>
         <span class="mr-2">Грид</span>
       </v-btn>
-      <v-btn href="/urlstate" text>
+      <v-btn href="urlstate" text>
         <span class="mr-2">Состояние в url</span>
       </v-btn>
     </v-app-bar>
