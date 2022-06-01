b063cc732424c66e087b37140c5df32f3501a1f0
Boris <toborto@yandex.ru>
2022-06-01 11:38:20 +0300

    Версионность

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,5 +1,16 @@
 <template>
   <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          readonly
+          outlined
+          v-model="appVersion"
+          hint="Версия клиентской части"
+          label="Версия клиентской части"
+        ></v-text-field>
+      </v-col>
+    </v-row>
     <v-row class="text-center">
       <v-col cols="12">
         <v-img :src="require('../assets/welcome.png')" contain />
@@ -9,9 +20,13 @@
 </template>
 
 <script>
+import packageInfo from "@/../package";
+
 export default {
   name: "WelcomePage",
 
-  data: () => ({}),
+  data: () => ({
+    appVersion: packageInfo.version,
+  }),
 };
 </script>
